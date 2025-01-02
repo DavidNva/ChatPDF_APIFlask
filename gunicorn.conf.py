4a2dcb815b491d95efd6485ed812369837cdcb60
David <dnava@krestonbsg.com.mx>
2025-01-02 10:06:42 -0600

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,12 @@
-import multiprocessing
-
 bind = "0.0.0.0:10000"
 worker_class = "gevent"
 workers = 1
-threads = 4
+threads = 2  # Reducido de 4 a 2
 timeout = 300
 keepalive = 2
+max_requests = 1
+max_requests_jitter = 5
+preload_app = True  # Añadido
 
-# Aumentar el límite de recursión
 import sys
 sys.setrecursionlimit(10000)
